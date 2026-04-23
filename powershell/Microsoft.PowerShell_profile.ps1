513f72d35c59ad06ea2ae1dbcc6d57b82f4ed7ac
Josef Koch <josef.koch97@gmail.com>
2026-04-23 09:05:06 +0200

    update ps1 profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -8,6 +8,11 @@ Set-PSReadLineOption -EditMode Windows `
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name co -Value copilot
 function vibe { copilot --yolo @args }
+function Update-Pwsh {
+	scoop update pwsh
+	Remove-Item "$env:SCOOP\apps\pwsh\current\profile.ps1", "$env:SCOOP\apps\pwsh\current\Microsoft.PowerShell_profile.ps1" -Force -ErrorAction SilentlyContinue
+}
+Set-Alias -Name uppwsh -Value Update-Pwsh
 
 # oh my posh
 # uses --print to omit group policy errors because oh-my-posh init is located in AppData
