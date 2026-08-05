583e1c8389f46f2a01be1e9768709740490d52fc
Josef Koch <josef.koch97@gmail.com>
2026-08-05 10:00:18 +0200

    changed from mise to fnm

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -14,8 +14,8 @@ Set-Alias -Name lg -Value lazygit
 Set-Alias -Name co -Value copilot
 function vibe { copilot --yolo @args }
 
-# mise
-(&mise activate pwsh) | Out-String | Invoke-Expression
+# fnm
+fnm env --use-on-cd | Out-String | Invoke-Expression
 
 # oh my posh
 oh-my-posh init pwsh --config "$env:XDG_CONFIG_HOME\oh-my-posh\star-win-term.omp.json" --print | Out-String | Invoke-Expression
